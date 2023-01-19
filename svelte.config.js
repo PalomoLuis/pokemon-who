836274f26d03df77085385286652a3a19a033316
Luis Carlos Palomo <42524903+PalomoLuis@users.noreply.github.com>
2023-01-19 14:14:54 -0300

    vercel adapter first try

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+			// if true, will deploy the app using edge functions
+			edge: false,
+			external: [],
+	   
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
 		  })
 	},
 	preprocess: vitePreprocess()
